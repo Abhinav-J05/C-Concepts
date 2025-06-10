2d9cc58be678f670a86fa833ceba77faf5580193
Abhinav Joshi <abhinavaccess5@gmail.com>
2025-06-10 12:58:16 +0530

    Nested structs

--- Structures & Unions/nested_struct.c
+++ Structures & Unions/nested_struct.c
@@ -3,6 +3,14 @@
 #include<stdint.h>
 #include<string.h>
 
+typedef struct 
+{
+    uint8_t field_a;
+    uint8_t field_b;
+    uint8_t field_c;
+
+} nested_struct;
+// Obviously needs to be defined before parent
 typedef struct
 {
     char name[10];
@@ -11,27 +19,27 @@ typedef struct
 
 } parent_struct;
 
-typedef struct 
-{
-    uint8_t field_a;
-    uint8_t field_b;
-    uint8_t field_c;
 
-} nested_struct;
 
 
 int main(){
     printf("\n");
 
-    parent_struct parent;
     nested_struct child;
+    parent_struct parent;
 
     // parent.name = "Abhinav"; wrong
     strcpy(parent.name, "Abhinav");
     parent.field_B = 5;
 
-    // parent.nested1
+    parent.nested1.field_a = 0;
+    parent.nested1.field_b = 1;
+    parent.nested1.field_c = 2;
 
+    printf("nested1: a=%u, b=%u, c=%u\n",
+           parent.nested1.field_a,
+           parent.nested1.field_b,
+           parent.nested1.field_c);
 
     return 0;
 }
