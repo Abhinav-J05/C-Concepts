b2a42c19f161900ec718330a4fec275c71b516a6
Abhinav Joshi <abhinavaccess5@gmail.com>
2025-06-15 12:45:39 +0530

    Utility file for file system operations.

---
+++ Interactive Command Line Interface/inc/fs_util.h
@@ -0,0 +1,30 @@
+/* 
+* -------------------------------------------------------------------
+* 
+* File        : fs_util.h
+* Author      : Abhinav Joshi
+* 
+* Description : Utility function declarations for file system.
+* 
+* Usage       : Include this header in modules that require utility 
+*               routines such as file creation and
+*               directory creation.
+* 
+* License     : MIT
+* 
+* -------------------------------------------------------------------
+*/
+
+
+
+//  File creation
+int create_file()
+{
+
+}
+
+// Dir creation
+int create_dir()
+{
+
+}
