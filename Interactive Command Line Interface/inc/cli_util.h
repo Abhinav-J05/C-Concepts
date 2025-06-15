e3d268a8fc237b70be113633976dfd32b614c62a
Abhinav Joshi <abhinavaccess5@gmail.com>
2025-06-15 12:46:01 +0530

    Utility file for Command line functions

---
+++ Interactive Command Line Interface/inc/cli_util.h
@@ -0,0 +1,22 @@
+/* 
+* -------------------------------------------------------------------
+* 
+* File        : cli_util.h
+* Author      : Abhinav Joshi
+* 
+* Description : Utility function declarations for command line interface.
+* 
+* Usage       : Include this header in modules that require utility 
+*               routines such as path operations.
+* 
+* License     : MIT
+* 
+* -------------------------------------------------------------------
+*/
+
+
+// Reset session state
+int reset_state()
+{
+    
+}
