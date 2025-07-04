95b3f7e03becaf6ef76816874b162f4291330ad1
Abhinav <86758548+Abhinav-J05@users.noreply.github.com>
2025-07-04 02:41:23 +0000

    Compilation error fix

--- Interactive Command Line Interface/src/parser.c
+++ Interactive Command Line Interface/src/parser.c
@@ -9,44 +9,44 @@
 
 int process_input(const char* input, session_state_t state)
 {
-    while(1)
-    {
+    // while(1)
+    // {
 
-        // create a file
-        if(strncmp(input, "touch ", 6) == 0)
-        {   
-            // TODO : decouple traverse logic to target path & file creation logic from here.
-            // char *args = input + 6;
+    //     // create a file
+    //     if(strncmp(input, "touch ", 6) == 0)
+    //     {   
+    //         // TODO : decouple traverse logic to target path & file creation logic from here.
+    //         // char *args = input + 6;
 
-            // // Split into filename and content
-            // char *filename = strtok(args, " ");
-            // char *file_content = strtok(NULL, "");
+    //         // // Split into filename and content
+    //         // char *filename = strtok(args, " ");
+    //         // char *file_content = strtok(NULL, "");
 
-            // if (filename == NULL)
-            // {
-            //     show_error("Filename missing for touch command.");
-            //     continue;
-            // }
+    //         // if (filename == NULL)
+    //         // {
+    //         //     show_error("Filename missing for touch command.");
+    //         //     continue;
+    //         // }
 
-            // if (file_content == NULL)
-            //     file_content = ""; 
+    //         // if (file_content == NULL)
+    //         //     file_content = ""; 
 
-            create_file(target_dir, false, false, filename, file_content);
-            continue;
-        }
+    //         create_file(target_dir, false, false, filename, file_content);
+    //         continue;
+    //     }
 
-        if(strncmp(input, "cd ", 3) == 0)
-        {
-            switch_session_state(&state, input + 3);
-            continue;
-        }
+    //     if(strncmp(input, "cd ", 3) == 0)
+    //     {
+    //         switch_session_state(&state, input + 3);
+    //         continue;
+    //     }
 
-        else
-        {
-            show_error("Invalid command !");
-            return 1;
-        }
-    }
+    //     else
+    //     {
+    //         show_error("Invalid command !");
+    //         return 1;
+    //     }
+    // }
 
     return 1;
 }
